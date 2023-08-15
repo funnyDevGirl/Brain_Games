fae4898d2c32242bff3accdaa58fafb366987192
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-15 20:06:30 +0300

    Modified App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,5 +3,8 @@ package hexlet.code;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String user = Cli.name();
+        System.out.println("Hello, " + user + "!");
     }
 }
