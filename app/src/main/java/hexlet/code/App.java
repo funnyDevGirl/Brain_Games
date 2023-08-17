e91df39a30336d9ebc35c634906071240e57315b
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-17 19:26:49 +0300

    Modified App.java, added evenGame

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,31 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String user = Cli.name();
-        System.out.println("Hello, " + user + "!");
+        System.out.println("Please enter the game number and press Enter.");
+        String[] points = {"0", "1", "2", "3", "4", "5", "6"};
+        System.out.println(points[1] + " - Greet");
+        System.out.println(points[2] + " - Even");
+        System.out.println(points[3] + " - Calc");
+        System.out.println(points[4] + " - GCD");
+        System.out.println(points[5] + " - Progression");
+        System.out.println(points[6] + " - Prime");
+        System.out.println(points[0] + " - Exit");
+        System.out.println("Your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.next();
+
+        if (choice.equals(points[1])) {
+            System.out.println(Games.greet());
+
+        } else if (choice.equals(points[2])) {
+            System.out.println(Games.greet());
+            Games.evenGame();
+
+        } else {
+            System.out.println("Incorrect command input. Goodbye.");
+        }
     }
 }
