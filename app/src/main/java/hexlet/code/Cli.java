66196a36c47c83b9219c2e55aa5d0e6558d9e210
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-18 10:37:39 +0300

    Modified Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class Cli {
     public static String name() {
         Scanner scanner = new Scanner(System.in);
-        String userName = scanner.next();
-        scanner.close();
-        return userName;
+        return scanner.next();
     }
 }
