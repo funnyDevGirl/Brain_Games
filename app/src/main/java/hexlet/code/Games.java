a4036cab14a83d06111b77a83e897073ce47044f
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-18 10:41:42 +0300

    Modified Games.java

--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -1,38 +1,63 @@
 package hexlet.code;
+//import java.util.Arrays;
 import java.util.Scanner;
 
 public class Games {
-    public static String greet() {
+    public static void greet() {
+        System.out.println("Welcome to the cdBrain Games!");
+        System.out.print("May I have your name? ");
+        String user = Cli.name();
+        System.out.println("Hello, " + user + "!");
+    }
+
+    /*public static void wrongAnswer() {
+        String test = Constants.USERNAME;
+        System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+        System.out.println("Let's try again, " + test + "!");
+    }
+
+    public static String endOfGame() {
+        String test = Constants.USERNAME;
+        return ("Congratulations, " + test + "!");
+    }*/
+
+    public static void evenGame() {
+        //greet
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String user = Cli.name();
-        return ("Hello, " + user + "!");
-    }
-    public static void evenGame() {
+        System.out.println("Hello, " + user + "!");
+        //2
         String[][] volumes = {{"15", "50", "33"}, {"no", "yes", "no"}};
-        var userName = Cli.name();
 
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        System.out.println("Your answer: ");
         Scanner scanner1 = new Scanner(System.in);
-        var count = 0;
+        int count = 0;
+        int i = 0;
+        int j = 0;
 
-        for (var i : volumes[0]) {
-            for (var j : volumes[1]) {
-                System.out.println("Question: " + i);
+        while (i < volumes[0].length) {
+            while (j < volumes[1].length) {
+                System.out.println("Question: " + volumes[0][i]);
+                System.out.print("Your answer: ");
                 String answer = scanner1.next();
-                if (answer.equals(j)) {
+                if (answer.equals(volumes[1][j])) {
                     System.out.println("Correct!");
                 } else {
+                    //Games.wrongAnswer();
                     System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                    System.out.println("Let's try again, " + userName + "!");
+                    System.out.println("Let's try again, " + user + "!");
                     break;
                 }
-                count++;
-                if (count == 2) {
-                    System.out.println("Congratulations, " + userName + "!");
+                count += 1;
+                i += 1;
+                j += 1;
+                if (count == 3) {
+                    //System.out.println(Games.endOfGame());
+                    System.out.println("Congratulations, " + user + "!");
                 }
             }
+            break;
         }
     }
 }
