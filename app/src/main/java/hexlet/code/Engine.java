c18fb71eae4cf10e79e704666a5c64f76d1396fe
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-21 17:35:20 +0300

    Updated Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,4 +7,23 @@ public class Engine {
         String user = Cli.name();
         System.out.println("Hello, " + user + "!");
     }
+
+    public static boolean checkingAnswer(boolean condition, Object rightAnswer, Object userAnswer) {
+
+        if (condition) {
+            System.out.println("Correct!");
+        } else {
+            System.out.print("'" + userAnswer + "' is wrong answer ;(. ");
+            System.out.println("Correct answer was '" + rightAnswer + "'.");
+            System.out.println("Let's try again, " + Cli.getName() + "!");
+            return true;
+        }
+        return false;
+    }
+
+    public static void congratulations(boolean condition) {
+        if (condition) {
+            System.out.println("Congratulations, " + Cli.getName() + "!");
+        }
+    }
 }
