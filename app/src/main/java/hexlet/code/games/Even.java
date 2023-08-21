a3126351ac2a92e55b4297313d161fd884a00323
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-21 17:33:54 +0300

    Updated Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,47 +1,34 @@
 package hexlet.code.games;
 
-import hexlet.code.Cli;
-
+import hexlet.code.Engine;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Even {
     public static void evenGame() {
-        //greet
-        //Engine.greet();
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String user = Cli.name();
-        System.out.println("Hello, " + user + "!");
+        Engine.greet();
 
-        //generate question and result
-        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int[] numbers = new int[3];
-        int i = 0;
-        int count = 0;
 
-        while (i < numbers.length) {
-            numbers[i] = random.nextInt(100);
-            String result = (numbers[i] % 2 == 0) ? "yes" : "no";
+        final int maxRound = 3;
+        int round = 0;
 
-            System.out.println("Question: " + numbers[i]);
+        while (round < maxRound) {
+            //generate question and answer
+            int number = random.nextInt(100);
+            String rightAnswer = (number % 2 == 0) ? "yes" : "no";
+
+            System.out.println("Question: " + number);
             System.out.print("Your answer: ");
-            String answer = scanner2.next();
-
-            if (answer.equals(result)) {
-                System.out.println("Correct!");
-            } else {
-                System.out.print("'" + answer + "'" + " is wrong answer ;(. ");
-                System.out.println("Correct answer was " + "'" + result + "'.");
-                System.out.println("Let's try again, " + user + "!");
+            String userAnswer = scanner.next();
+            //checking result
+            if (Engine.checkingAnswer(userAnswer.equals(rightAnswer), rightAnswer, userAnswer)) {
                 break;
             }
-            i += 1;
-            count += 1;
-            if (count == 3) {
-                System.out.println("Congratulations, " + user + "!");
-            }
+            round += 1;
+            Engine.congratulations(round == maxRound);
         }
     }
 }
