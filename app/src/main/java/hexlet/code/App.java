77d45c2ded47b383580137ca645ab5b35cc20b75
Alina Tarasova <alishka.tarasova@gmail.com>
2023-08-25 16:10:34 +0300

    Update Engine.java and App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,6 +7,7 @@ import hexlet.code.games.Calc;
 import hexlet.code.games.Prime;
 import hexlet.code.games.Progression;
 
+
 public class App {
     public static void main(String[] args) {
         System.out.println("""
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,11 +1,32 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Engine {
     public static void greet() {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String user = Cli.name();
         System.out.println("Hello, " + user + "!");
+    }
+
+    public static void userInteraction(String[][] roundArr) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int maxRound = 3;
+
+        int round = 0;
+
+        while (round < maxRound) {
+            System.out.println("Question: " + roundArr[round][0]);
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.next();
+            if (Engine.checkingAnswer(userAnswer.equals(roundArr[round][1]), roundArr[round][1], userAnswer)) {
+                break;
+            }
+            round += 1;
+            Engine.congratulations(round == maxRound);
+        }
     }
 
     public static boolean checkingAnswer(boolean condition, Object rightAnswer, Object userAnswer) {
